86100f2b603248d400d3644f4251a8d01ad98dee
yokazecloud <yokazecloud@gmail.com>
2023-06-05 11:16:13 +0900

    auto generate output file name

--- src/Video_to_audio.py
+++ src/Video_to_audio.py
@@ -2,9 +2,9 @@ from moviepy.editor import VideoFileClip, AudioFileClip
 
 def convert(
     *,
-    input: str, 
-    output: str
+    input: str
 ) -> None:
+    OUTPUT: str = f"{input}_audio.mp3"
     video: VideoFileClip = VideoFileClip(input)
     audio: AudioFileClip = video.audio
-    audio.write_audiofile(output)
+    audio.write_audiofile(OUTPUT)
